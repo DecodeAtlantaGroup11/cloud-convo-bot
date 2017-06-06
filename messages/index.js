bf035b76c5d15e81744d64566ca14bcd561abcef
Mayoor Rai <mayoor.rai@ge.com>
2017-06-06 19:27:48 -0400

    console logs

--- messages/index.js
+++ messages/index.js
@@ -30,7 +30,9 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+console.log(recognizer);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+console.log(intents);
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 test text for CI
